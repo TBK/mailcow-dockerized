b5d15aa2d00763f9cca8e7581361350f4945964b
TBK <tbk@jjtc.dk>
2018-09-21 06:29:52 +0200

    [Unbound] Switch to posix sh

--- data/Dockerfiles/unbound/docker-entrypoint.sh
+++ data/Dockerfiles/unbound/docker-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Setting console permissions..."
 chown root:tty /dev/console
 chmod g+rw /dev/console
 echo "Receiving anchor key..."
-/usr/sbin/unbound-anchor -a /etc/unbound/trusted-key.key
+unbound-anchor -a /etc/unbound/trusted-key.key
 echo "Receiving root hints..."
 curl -#o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
 /usr/sbin/unbound-control-setup
